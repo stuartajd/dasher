036ba0a5d3497a71d1f4d117d64729e5126b5de9
StuartD <me@stuartd.co.uk>
2016-09-28 21:48:30 +0100

    Removed Unrequired PHP File

--- server/environ.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /*
-     * environ.php
-     * @description: Used to store all environment variables that are used throughout the server
-     * @description: including the database information.
-     */
-    
-    $_ENV = array();
-    
