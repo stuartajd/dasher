c3ba161209ae6bc4c4cea7b4dff0bd9d92e16d71
Stuart Davidson <stuartd96@live.co.uk>
2017-03-30 00:35:31 +0100

    Added comments

--- private/dasher.module.js
+++ private/dasher.module.js
@@ -1,7 +1,14 @@
 /**
- Dasher Module
+ * Dasher - dasher.module.js
+ *
+ * The client side scripts for the Dasher Package.
+ *
+ * @author: UP772629
  */
 
+/**
+ * Clears the screen, then display the Dasher ASCII code.
+ */
 function dashboardText(){
     // Clear the display screen
     process.stdout.write('\x1Bc');
